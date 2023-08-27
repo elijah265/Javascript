8393c85f8ffe610336ae6072d0b51ab6c6931d54
unknown <elijahfx43@gmail.com>
2023-08-27 22:40:30 +0100

    javascript type conversion

--- Type Conversions.js
+++ Type Conversions.js
@@ -19,8 +19,8 @@ console.log(typeof num) // number
 console.log(Boolean(1));//true
 console.log(Boolean(0));//false
 console.log(Boolean('hello'));// true
-console.log(Boolean(''));
-console.log(Boolean("0"));
+console.log(Boolean(''));// false
+console.log(Boolean("0"));// true
 
 
 
