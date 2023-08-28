ec804b8debf51909122abd30da90be8bf317b56c
unknown <elijahfx43@gmail.com>
2023-08-28 02:09:35 +0100

    javascript variable

---
+++ Variable.js
@@ -0,0 +1,50 @@
+/*  A variable is a “named storage” for data. We can use variables to store goodies, visitors, and other data.
+   To create a variable in JavaScript, use the var, let and const keywords.
+   var is an outdated method of declearing a variable.
+   let and cost and the way ways variablem is declare
+*/
+
+let massage = 'Hello';//declearing and assigning
+let userName // declearing
+userName = "Elijah"; // asigning the string to userName
+console.log(massage); // Hello
+console.log(userName); // Elijah
+ // we can declear multiple Variables
+
+ let userFirstName = "Elijah"; // Elijah
+     userLasttName = "Peter"; // Peter
+     age = 26; // 26
+     country = 'Nigeria'; // Nigeria
+     city = "Abuja"; // Abuja
+
+     console.log(userFirstName);
+     console.log(userLasttName);
+     console.log(age);
+     console.log(country);
+     console.log(city);
+
+// We can also declare two variables and copy data from one into the other.
+let hello = "hello, World!"
+let msg;
+msg = hello; // hello, World!
+console.log(msg);
+
+// A repeated declaration of the same variable is an error:
+/*
+let Name  = 'Eliajh';
+let Name = 'Peter'; // Syntaxerror: 'massage' has been  already been declared
+*/
+
+
+
+// costant:o declare a constant (unchanging) variable, use const instead of let:
+//Variables declared using const are called “constants”. They cannot be reassigned. An attempt to do so would cause an error:
+const myName = "Elijah";
+myBirthday = "14.02.1997";
+
+
+
+
+
+
+
